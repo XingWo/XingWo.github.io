09646fe702cd6116deb54c5a61f82f9c37307006
XingWo <107658852+XingWo@users.noreply.github.com>
2022-06-20 21:14:37 +0800

    2.1
    删除重复

--- 小车案例/jq内容方法/js/che.js
+++ 小车案例/jq内容方法/js/che.js
@@ -1,24 +1,31 @@
+var nr1=document.getElementById("content_right");
 function btn(num){
 	$(".txt2").removeClass("txt2x");
-	$("#txt"+num).addClass("txt2x")
+	$("#txt"+num).addClass("txt2x");
 }
 $(document).ready(function(){
 	$("#txt0").click(function(){
+		nr1.className="content_right animated rotateIn";
 		 $(".content_right").html('<div class="che1"><div class="che"></div><p>这是产品名称</p></div><div class="che1"><div class="che"></div><p>这是产品名称</p></div><div class="che1"><div class="che"></div><p>这是产品名称</p></div><div class="che1"><div class="che"></div><p>这是产品名称</p></div><div class="che1"><div class="che"></div><p>这是产品名称</p></div><div class="che1"><div class="che"></div><p>这是产品名称</p></div>')
 	 });
 	 $("#txt1").click(function(){
+		 nr1.className="content_right animated fadeIn";
 	 	 $(".content_right").html('<div class="che2"><div class="che"></div><p>这是产品名称</p></div><div class="che2"><div class="che"></div><p>这是产品名称</p></div><div class="che2"><div class="che"></div><p>这是产品名称</p></div><div class="che2"><div class="che"></div><p>这是产品名称</p></div><div class="che2"><div class="che"></div><p>这是产品名称</p></div><div class="che2"><div class="che"></div><p>这是产品名称</p></div>')
 	  });
 	  $("#txt2").click(function(){
+		  nr1.className="content_right animated fadeInUp";
 	  		 $(".content_right").html('<div class="che3"><div class="che"></div><p>这是产品名称</p></div><div class="che3"><div class="che"></div><p>这是产品名称</p></div><div class="che3"><div class="che"></div><p>这是产品名称</p></div><div class="che3"><div class="che"></div><p>这是产品名称</p></div><div class="che3"><div class="che"></div><p>这是产品名称</p></div><div class="che3"><div class="che"></div><p>这是产品名称</p></div>')
 	  	 });
 		 $("#txt3").click(function(){
+			 nr1.className="content_right animated fadeInDown";
 		 	 $(".content_right").html('<div class="che4"><div class="che"></div><p>这是产品名称</p></div><div class="che4"><div class="che"></div><p>这是产品名称</p></div><div class="che4"><div class="che"></div><p>这是产品名称</p></div><div class="che4"><div class="che"></div><p>这是产品名称</p></div><div class="che4"><div class="che"></div><p>这是产品名称</p></div><div class="che4"><div class="che"></div><p>这是产品名称</p></div>')
 		  });
 		  $("#txt4").click(function(){
+			  nr1.className="content_right animated bounceIn";
 		  	 $(".content_right").html('<div class="che5"><div class="che"></div><p>这是产品名称</p></div><div class="che5"><div class="che"></div><p>这是产品名称</p></div><div class="che5"><div class="che"></div><p>这是产品名称</p></div><div class="che5"><div class="che"></div><p>这是产品名称</p></div><div class="che5"><div class="che"></div><p>这是产品名称</p></div><div class="che5"><div class="che"></div><p>这是产品名称</p></div>')
 		   });
 		   $("#txt5").click(function(){
+			   nr1.className="content_right animated rollIn";
 		   	 $(".content_right").html('<div class="che6"><div class="che"></div><p>这是产品名称</p></div><div class="che6"><div class="che"></div><p>这是产品名称</p></div><div class="che6"><div class="che"></div><p>这是产品名称</p></div><div class="che6"><div class="che"></div><p>这是产品名称</p></div><div class="che6"><div class="che"></div><p>这是产品名称</p></div><div class="che6"><div class="che"></div><p>这是产品名称</p></div>')
 		    });
 });

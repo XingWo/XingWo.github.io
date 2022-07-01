d062acd91db56fbcff215608e8f128475739cc43
XingWo <107658852+XingWo@users.noreply.github.com>
2022-07-01 13:02:47 +0800

    3.0.5
    续上

--- js/index.js
+++ js/index.js
@@ -888,7 +888,7 @@ function qtbtn(num){
 		nr2.innerHTML=`
 		<div id="QT2" style="height: 100%;width: 100%;z-index: 3;box-shadow: 0 0 0.3vh #98dbff;   border-radius: 8px;overflow: hidden;position: absolute;top: 0;left: 0;">
 			<p style="margin-top: 20vh;">网址已复制到粘贴版，若无法嵌入式跳转，可直接在网址导航粘贴访问</p>
-			<iframe id="QT2_nr" src="http://music.wandhi.com" width="100%" frameborder="0"  style="margin-top: -430px;background:rgba(152, 219, 255, 0.38)"></iframe>
+			<iframe id="QT2_nr" src="http://music.wandhi.com" width="100%" frameborder="0"  style="position: absolute;left: 0;top: -430px;background:rgba(152, 219, 255, 0.38)"></iframe>
 		</div>
 		`;
 		var wandhi = $("#qtbtn2").text();//获取文本
